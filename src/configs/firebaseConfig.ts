f6c56ff4041a9ec22cb49be87960fa2c3107ce0a
FennecFoxy11 <yositamook@gmail.com>
2022-03-20 16:38:32 +0700

    Revert "Merge pull request #1 from flarious/mook-server"
    This reverts commit c788b841424ec65d22c4de1099469cd691ccc1e3, reversing
    changes made to 17cbf2ac1ecd9a67255485cb6523a7d2f100c8ca.

--- src/configs/firebaseConfig.ts
+++ src/configs/firebaseConfig.ts
@@ -1,6 +1,5 @@
 import * as serviceAccount from 'serviceAccountKey.json';
 
-
 const firebaseConfig = {
     type: serviceAccount.type,
     projectId: serviceAccount.project_id,
@@ -11,7 +10,7 @@ const firebaseConfig = {
     authUri: serviceAccount.auth_uri,
     tokenUri: serviceAccount.auth_uri,
     authProviderX509CertUrl: serviceAccount.auth_provider_x509_cert_url,
-    clientC509CertUrl: serviceAccount.client_x509_cert_url,
+    clientC509CertUrl: serviceAccount.client_x509_cert_url
 }
 
 
