1eddd97b61b5fdfbac5f1dc6d8cfdc5c58dc0df9
Nattawut Klinsawas <nattawutklinsawas@gmail.com>
2022-04-01 16:48:19 +0700

    Change way event's date is handled

--- src/api/events/events.service.ts
+++ src/api/events/events.service.ts
@@ -17,16 +17,14 @@ export class EventsService {
 
     async insert(name, host, body, thumbnail, location, date: string) {
         const interested_amount = 0;
-        const temp = date.split('/').reverse();
-        const eventDate = new Date(parseInt(temp[0]), parseInt(temp[1]) - 1, parseInt(temp[2]));
+        const eventDate = new Date(date);
         const report_amount = 0;
 
         await this.repository.insert(name, host, body, thumbnail, location, interested_amount, eventDate, report_amount);
     }
 
     async update(eventID, name, body, thumbnail, location, date) {
-        const temp = date.split('/').reverse();
-        const eventDate = new Date(parseInt(temp[0]), parseInt(temp[1]) - 1, parseInt(temp[2]));
+        const eventDate = new Date(date);
         eventID = eventID.split("_")[1];
         
         await this.repository.update(eventID, name, body, thumbnail, location, eventDate);
